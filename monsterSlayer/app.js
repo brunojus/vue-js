aa0c7ccf67319f25c0a050830a4e8e0aa6941f40
Bruno Justino <ti@qualconcurso.com.br>
2016-12-02 20:39:26 -0200

    Finishing the log

--- monsterSlayer/app.js
+++ monsterSlayer/app.js
@@ -3,7 +3,8 @@ new Vue({
 	data:{
 		playerHealth:100,
 		monsterHealth:100,
-		gameIsRunning:false
+		gameIsRunning:false,
+		turns: []
 	},
 
 	methods:{
@@ -11,11 +12,17 @@ new Vue({
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.turns = [];
 		},
 
 		attack: function(){
- 
-			this.monsterHealth -= this.calculateDamage(3,10); ;
+ 			
+ 			var damage = this.calculateDamage(3,10); 
+			this.monsterHealth -= damage; 
+			this.turns.unshift({
+				isPlayer:true,
+				text: 'Player hits Monster for '+ damage
+			});
 
 			if(this.checkWin()){
 				return;
@@ -24,7 +31,14 @@ new Vue({
 		},
 
 		specialAttack: function(){
-			this.monsterHealth -= this.calculateDamage(10,20); ;
+
+			var damage = this.calculateDamage(10,20)
+			this.monsterHealth -= damage; 
+
+			this.turns.unshift({
+				isPlayer:true,
+				text: 'Player hits Monster hard for '+ damage
+			});
 
 			if(this.checkWin()){
 				return;
@@ -41,6 +55,12 @@ new Vue({
 			else{
 				this.playerHealth = 100;
 			}
+
+
+			this.turns.unshift({
+				isPlayer:true,
+				text: 'Player heals for 10'
+			});
 			
 			this.monsterAttacks();
 		},
@@ -51,8 +71,16 @@ new Vue({
 		},
 
 		monsterAttacks: function(){
-			this.playerHealth -= this.calculateDamage(5,12);
+
+			var damage = this.calculateDamage(5,12);
+			this.playerHealth -= damage;
 			this.checkWin();
+
+			this.turns.unshift({
+				isPlayer:false,
+				text: 'Monster hits Player for '+  damage
+			});
+
 		},
 
 		calculateDamage:function(min,max){
